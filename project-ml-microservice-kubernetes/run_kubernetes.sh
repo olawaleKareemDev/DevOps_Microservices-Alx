d4abec72d7478697c6e6cc24426a342c35385e7d
olawaleKareemDev <olawalekareemdev@gmail.com>
2022-09-04 13:05:00 +0000

    get k8s logs

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
-
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+ dockerpath=olawale100/alx-devops:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run flask-app --image=olawale100/alx-devops:latest  --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+sleep 100
+kubectl port-forward flask-app 8000:80
 
